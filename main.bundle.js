20ef11e3b099580244d5a6403242b3f402d6979c
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-16 14:57:46 -0600

    Update main bundle

--- main.bundle.js
+++ main.bundle.js
@@ -46,38 +46,7 @@
 
 	'use strict';
 
-	///////////////////////////// FOOD.HTML PAGE //////////////////////////////////////
-
-	// GET request
-	var getAllFoods = function getAllFoods() {
-	  $('#food-list').html('');
-	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods').then(handleResponse).then(getFood).catch(errorLog);
-	};
-
-	var getFood = function getFood(foods) {
-	  return foods.forEach(function (food) {
-	    $('#food-list').append('\n    <article class="food-info">\n    <p class="name">' + food.name + '</p>\n    <p class="calories">' + food.calories + '</p>\n    <button class="updateFood-btn" id="' + food.id + '">Edit</button>\n    <button class="deleteFood-btn" id="' + food.id + '">Delete</button>\n    ');
-	  });
-	};
-
-	// POST request
-	var addNewFood = function addNewFood() {
-	  var newFoodName = $('#newfoodName').val();
-	  var newFoodCalories = $('#newfoodCalories').val();
-	  var newFoodInfo = { food: { name: newFoodName, calories: newFoodCalories } };
-
-	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods', newFoodPayload(newFoodInfo)).then(handleResponse).then(getAllFoods).then(clearInput).catch(errorLog);
-	};
-
-	var newFoodPayload = function newFoodPayload(body) {
-	  return {
-	    method: 'POST',
-	    headers: { 'Content-Type': 'application/json' },
-	    body: JSON.stringify(body)
-	  };
-	};
-
-	// HELPERS
+	// -------------------------------------------------------------------- HELPER FUNCTIONS
 	var clearInput = function clearInput() {
 	  $('.input').val('');
 	};
@@ -100,40 +69,175 @@
 	  console.error({ error: error });
 	};
 
+	// -------------------------------------------------------------------- FOOD.HTML PAGE 
+
+	// GET 
+	var getAllFoods = function getAllFoods() {
+	  $('#food-list').html('');
+	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods').then(handleResponse).then(getFood).catch(errorLog);
+	};
+
+	var getFood = function getFood(foods) {
+	  return foods.forEach(function (food) {
+	    $('#food-list').append('\n    <article class="food-info">\n    <p class="name" contenteditable="false" id="name-' + food.id + '"=>' + food.name + '</p>\n    <p class="calories" contenteditable="false" id="calories-' + food.id + '"=>' + food.calories + '</p>\n    <div class="tooltip">\n      <button type="button" class="updateFood-btn" id="' + food.id + '">Edit</button>\n      <span class="tooltip-update">Click edit, click on field, make changes, click save.</span>\n    </div>\n    <div class="tooltip">\n      <button type="button" class="deleteFood-btn" id="' + food.id + '">Delete</button>\n     <span class="tooltip-delete">Food can only be deleted if it is not linked to a meal. Visit the meal page to remove from meals.</span>\n    </div>\n    ');
+	  });
+	};
+
+	// POST 
+	var postFood = function postFood() {
+	  var newFoodName = $('#newfoodName').val();
+	  var newFoodCalories = $('#newfoodCalories').val();
+	  var body = { food: { name: newFoodName, calories: newFoodCalories } };
+
+	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods', postPayload(body)).then(handleResponse).then(getAllFoods).then(clearInput).catch(errorLog);
+	};
+
+	var postPayload = function postPayload(body) {
+	  return {
+	    method: 'POST',
+	    headers: { 'Content-Type': 'application/json' },
+	    body: JSON.stringify(body)
+	  };
+	};
+
+	// PATCH 
+	$("#food-list").on('click', '.updateFood-btn', function (event) {
+	  var foodId = event.currentTarget.id;
+	  var nameField = document.getElementById('name-' + foodId);
+	  var caloriesField = document.getElementById('calories-' + foodId);
+	  var buttonText = document.getElementById('' + foodId).innerHTML;
+
+	  if (buttonText == "Edit") {
+	    document.getElementById('' + foodId).innerHTML = "Save";
+	    nameField.contentEditable = "true";
+	    caloriesField.contentEditable = "true";
+	  } else if (buttonText == "Save") {
+	    document.getElementById('' + foodId).innerHTML = "Edit";
+	    nameField.contentEditable = "false";
+	    caloriesField.contentEditable = "false";
+
+	    var updatedName = nameField.innerText;
+	    var updatedCals = caloriesField.innerText;
+	    patchFood(foodId, updatedName, updatedCals);
+	  }
+	});
+
+	var patchFood = function patchFood(foodId, updatedName, updatedCals) {
+	  var body = { food: { name: updatedName, calories: updatedCals } };
+
+	  fetch('https://fast-meadow-36413.herokuapp.com//api/v1/foods/' + foodId + '/', patchPayload(body)).then(function (response) {
+	    return handleResponse(response);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var patchPayload = function patchPayload(body) {
+	  return {
+	    method: 'PUT',
+	    headers: { 'Content-Type': 'application/json' },
+	    body: JSON.stringify(body)
+	  };
+	};
+
+	// SEARCH
+	$('#search').keyup(function () {
+	  var letters = $('#search').val().toLowerCase();
+	  $('.food-info').each(function () {
+	    var string = this.innerText.substring(0, 10).toLowerCase();
+	    if (string.indexOf(letters) > -1) {
+	      $(this).show();
+	    } else {
+	      $(this).hide();
+	    }
+	  });
+	});
+
+	// EVENTS
 	getAllFoods();
+	$('#add-food-btn').on('click', postFood);
 
-	$('#newFood-btn').on('click', addNewFood);
-	$('#createFood-btn').on('click', addNewFood);
+	// -------------------------------------------------------------------- MEAL.HTML PAGE 
 
-	///////////////////////////// MEAL.HTML PAGE //////////////////////////////////////
-
-	// GET request
+	// GET
 	var getAllFoodsCheck = function getAllFoodsCheck() {
 	  $('#allFoods').html('');
-	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods').then(handleResponse).then(getFoodCheck).catch(errorLog);
+	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/foods').then(handleResponse).then(appendFoodToList).catch(errorLog);
 	};
 
-	var getFoodCheck = function getFoodCheck(foods) {
+	var appendFoodToList = function appendFoodToList(foods) {
 	  return foods.forEach(function (food) {
-	    $('#allFoods').append('\n      <div class="food">\n      <p class="name">' + food.name + '</p>\n      <p class="calories">' + food.calories + '</p>\n      <input type="checkbox" class="food-chk">\n    ');
+	    $('#food-list-for-meals').append('\n      <div class="food-info">\n      <p class="name">' + food.name + '</p>\n      <p class="calories">' + food.calories + '</p>\n      <input type="checkbox" class="food-chk" id="' + food.id + '">\n    ');
 	  });
 	};
 
-	var getAllMealsBtn = function getAllMealsBtn() {
-	  $('#allMeals').html('');
-	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/meals').then(handleResponse).then(getMealBtn).catch(errorLog);
+	var getAllMealsForButtons = function getAllMealsForButtons() {
+	  $('#meal-buttons').html('');
+	  fetch('https://rails-quantified-self.herokuapp.com/api/v1/meals').then(handleResponse).then(appendMealToButton).catch(errorLog);
 	};
 
-	var getMealBtn = function getMealBtn(meals) {
+	var appendMealToButton = function appendMealToButton(meals) {
 	  return meals.forEach(function (meal) {
-	    $('#allMeals').append('\n     <form id="meal">\n      <br>\n      <input type="submit" class="meal-btn" id=' + meal.id + ' value=' + meal.name + '>\n      </form> \n    ');
+	    $('#meal-buttons').append('\n     <div id="meal">\n      <input type="submit" class="meal-name-btn" id=' + meal.id + ' value=' + meal.name + '>\n      </div> \n    ');
 	  });
 	};
 
-	getAllFoodsCheck();
-	getAllMealsBtn();
+	$("#meal-buttons").on('click', '.meal-name-btn', function (event) {
+	  var mealId = event.currentTarget.id;
+	  var foodId = function foodId() {
+	    return this.id;
+	  };
+	  var foodsChecked = $(":checkbox:checked").map(foodId).get();
 
-	///////////////////////////// DIARY.HTML PAGE //////////////////////////////////////
+	  if (foodsChecked.length != 0) {
+	    alert("Check your diary to see the updates!");
+	    postFoodsToMeal(foodsChecked, mealId);
+	  } else {
+	    alert("You must select a food first.");
+	  }
+	});
+
+	var postFoodsToMeal = function postFoodsToMeal(foodsChecked, mealId) {
+	  var length = foodsChecked.length;
+	  for (var i = 0; i < length; i++) {
+	    fetch('https://fast-meadow-36413.herokuapp.com/api/v1/meals/' + mealId + '/foods/' + foodsChecked[i], { method: "POST" }).then(handleResponse).catch(errorLog);
+	  }
+	};
+
+	// -------------------------------------------------------------------- DIARY.HTML PAGE 
+
+	// GET
+	var getDiaryMeals = function getDiaryMeals() {
+	  $('#meal-columns').html('');
+	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/meals').then(handleResponse).then(getMealInfo).catch(errorLog);
+	};
+
+	var getMealInfo = function getMealInfo(meals) {
+	  return meals.forEach(function (meal) {
+	    $('#meal-columns').append('\n      <section class="meal-info">' + meal.name + '</section>\n    ');
+	    populateFoodsPerMeal(meal);
+	  });
+	};
+
+	var populateFoodsPerMeal = function populateFoodsPerMeal(meal) {
+	  return meal.foods.forEach(function (food) {
+	    $('.meal-info').append('\n    <article class="food-info">\n    <p class="name">' + food.name.substring(0, 8) + '</p>\n    <p class="calories">' + food.calories + '</p>\n    <p class="delete-btn" id=' + food.id + ' aria-label="Delete Food"><i class="fa fa-trash-o fa-lg" id=' + meal.id + '></i></p>\n    </article>\n    ');
+	  });
+	};
+
+	$("#meal-columns").on('click', '.delete-btn', function (event) {
+	  var foodId = event.currentTarget.id;
+	  var mealId = event.target.id;
+	  deleteMealFood(mealId, foodId);
+	});
+
+	var deleteMealFood = function deleteMealFood(mealId, foodId) {
+	  fetch('https://fast-meadow-36413.herokuapp.com/api/v1/meals/' + mealId + '/foods/' + foodId, { method: "DELETE" }).then(handleResponse).then(getDiaryMeals).catch(errorLog);
+	};
+
+	getDiaryMeals();
+	getAllFoodsCheck();
+	getAllMealsForButtons();
 
 /***/ })
 /******/ ]);
